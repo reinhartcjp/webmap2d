99565bf2716c82b561b8b52132b7078c9b687050
reinhartcjp <81622295+reinhartcjp@users.noreply.github.com>
2021-06-10 23:30:45 +0700

    Add files via upload

---
+++ qgis2web_2021_06_10-22_46_53_636333/data/EarlyWarningSystem_8.js
@@ -0,0 +1,15 @@
+var json_EarlyWarningSystem_8 = {
+"type": "FeatureCollection",
+"name": "EarlyWarningSystem_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "EWS 6", "Gambar": "F:\\Users\\User\\Documents\\Undip\\Semester 6\\Sistem Informasi Perencanaan\\Tubes\\Foto Survei\\Meteseh.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.4687014, -7.0650022 ] } },
+{ "type": "Feature", "properties": { "Name": "EWS 1", "Gambar": "F:\\Users\\User\\Documents\\Undip\\Semester 6\\Sistem Informasi Perencanaan\\Tubes\\Foto Survei\\Ngaliyan 4.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.3073343, -6.9710085 ] } },
+{ "type": "Feature", "properties": { "Name": "EWS 2", "Gambar": "F:\\Users\\User\\Documents\\Undip\\Semester 6\\Sistem Informasi Perencanaan\\Tubes\\Foto Survei\\Ngaliyan 3.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.3055815, -6.9754582 ] } },
+{ "type": "Feature", "properties": { "Name": "EWS 3", "Gambar": "F:\\Users\\User\\Documents\\Undip\\Semester 6\\Sistem Informasi Perencanaan\\Tubes\\Foto Survei\\Ngaliyan Mts.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.3194518, -7.0059748 ] } },
+{ "type": "Feature", "properties": { "Name": "EWS 4", "Gambar": "F:\\Users\\User\\Documents\\Undip\\Semester 6\\Sistem Informasi Perencanaan\\Tubes\\Foto Survei\\Ngaliyan Jbt.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.3429016, -6.9972361 ] } },
+{ "type": "Feature", "properties": { "Name": "EWS 5", "Gambar": "F:\\Users\\User\\Documents\\Undip\\Semester 6\\Sistem Informasi Perencanaan\\Tubes\\Foto Survei\\Genuk.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.4963357, -6.9686928 ] } },
+{ "type": "Feature", "properties": { "Name": "EWS 7", "Gambar": "F:\\Users\\User\\Documents\\Undip\\Semester 6\\Sistem Informasi Perencanaan\\Tubes\\Foto Survei\\Puncang Gading 2.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.4843491, -7.0404592 ] } },
+{ "type": "Feature", "properties": { "Name": "Bendungan Pucang Gading 2", "Gambar": "F:\\Users\\User\\Documents\\Undip\\Semester 6\\Sistem Informasi Perencanaan\\Tubes\\Foto Survei\\Puncang Gading.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.484251, -7.0398719 ] } }
+]
+}
